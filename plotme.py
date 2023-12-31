fade834e7c1eeeca6bdb0def542f0a6e3b8d161a
Christopher Albert <albert@alumni.tugraz.at>
2023-12-31 14:05:28 +0000

    Fewer toroidal harmonics

--- plotme.py
+++ plotme.py
@@ -2,21 +2,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("fort.10")
+data = np.loadtxt("delta_phi.out")
 
 data = data.reshape((75, 64, 50))
 
 # Plot 2D slice as contour
 plt.figure()
-plt.contour(data[1, :, :])
+plt.contour(data[25, :, :])
 plt.colorbar()
 
 
 plt.figure()
-plt.contour(data[:, :, 10])
+plt.contour(data[:, :, 25])
 plt.colorbar()
 
 plt.figure()
-plt.plot(data[10, :, 32], 'o-')
+plt.plot(data[25, :, 25], 'o-')
 
 # %%
