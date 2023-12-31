2e2195956adffef17601a5f060f77c9a64fdf435
Christopher Albert <albert@alumni.tugraz.at>
2023-12-31 08:48:36 +0000

    Plots working

--- plotme.py
+++ plotme.py
@@ -8,7 +8,15 @@ data = data.reshape((75, 32, 50))
 
 # Plot 2D slice as contour
 plt.figure()
+plt.contour(data[1, :, :])
+plt.colorbar()
+
+
+plt.figure()
 plt.contour(data[:, :, 10])
 plt.colorbar()
+
+plt.figure()
+plt.plot(data[10, :, 32], 'o-')
 
 # %%
