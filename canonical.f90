07178dc53b16d470d560d8a935a6d8b027c8b981
Christopher Albert <albert@alumni.tugraz.at>
2024-01-16 01:51:57 +0100

    Splining

--- canonical.f90
+++ canonical.f90
@@ -4,6 +4,9 @@ module canonical
     implicit none
 
     real(8), parameter :: pi = atan(1.d0)*4.d0
+    real(8), parameter :: twopi = atan(1.d0)*2.d0
+
+    integer, parameter :: nper = 1  ! Number of periods (TODO: make thi_r a variable)
     integer, parameter :: spl_order = 5
 
     real(8) :: phi_c, z_c  ! Temporary variables for the ODE integration
@@ -18,22 +21,22 @@ module canonical
 
 contains
 
-    subroutine init(n_r_, n_phi_, n_z_)
+    subroutine init(n_r_, n_z_, n_phi_)
         use my_little_magfie, only : init_magfie => init
 
-        integer, intent(in) :: n_r_, n_phi_, n_z_  ! Number of grid points
+        integer, intent(in) :: n_r_, n_z_, n_phi_  ! Number of grid points
         integer :: k
 
         call init_magfie
 
         n_r = n_r_
-        n_phi = n_phi_
         n_z = n_z_
+        n_phi = n_phi_
 
         ! Grid spacing
         h_r = (rmax-rmin)/dble(n_r-1)
-        h_phi = 2.d0*pi/dble(n_phi-1)
         h_z = (zmax-zmin)/dble(n_z-1)
+        h_phi = twopi/dble(n_phi-1)
 
         ! Spline derivative factors
         do k=1,spl_order+1
@@ -44,22 +47,23 @@ contains
     end subroutine init
 
 
-    subroutine spline_transformation(spl_data)
+    subroutine construct_splines(spl_data)
         real(8), intent(inout) :: spl_data(:,:,:,:,:,:,:)
 
         real(8), dimension(0:spl_order,n_r)   :: splcoe_r
-        real(8), dimension(0:spl_order,n_phi) :: splcoe_phi
         real(8), dimension(0:spl_order,n_z)   :: splcoe_z
+        real(8), dimension(0:spl_order,n_phi) :: splcoe_phi
+
         integer :: n_data
-        integer :: i_r, i_p, i_z          ! Loop counters for grid
-        integer :: is_z, is_phi, i_data, k  ! Loop counters for splines
+        integer :: i_r, i_z, i_phi            ! Loop counters for grid
+        integer :: i_r_z, i_r_phi, i_data, k  ! Loop counters for splines
 
         n_data = size(spl_data, 1)
 
         ! Spline over $\varphi$
         do i_r=1,n_r
         do i_z=1,n_z
-            do i_data=1,3
+            do i_data=1,n_data
                 splcoe_phi(0,:)=spl_data(i_data,1,1,1,i_r,i_z,:)
                 call spl_per(spl_order,n_phi,h_phi,splcoe_phi)
                 do k=1,spl_order
@@ -71,13 +75,13 @@ contains
 
         ! Spline over $Z$
         do i_r=1,n_r
-        do i_p=1,n_phi
-            do is_phi=1,spl_order+1
-                do i_data=1,3
-                    splcoe_z(0,:)=spl_data(i_data,1,1,is_phi,i_r,:,i_p)
+        do i_phi=1,n_phi
+            do i_r_phi=1,spl_order+1
+                do i_data=1,n_data
+                    splcoe_z(0,:)=spl_data(i_data,1,1,i_r_phi,i_r,:,i_phi)
                     call spl_reg(spl_order,n_z,h_z,splcoe_z)
                     do k=1,spl_order
-                        spl_data(i_data,1,k+1,is_phi,i_r,:,i_p)=splcoe_z(k,:)
+                        spl_data(i_data,1,k+1,i_r_phi,i_r,:,i_phi)=splcoe_z(k,:)
                     enddo
                 enddo
             enddo
@@ -86,14 +90,14 @@ contains
 
         ! Spline over $R$
         do i_z=1,n_z
-        do i_p=1,n_phi
-            do is_z=1,spl_order+1
-            do is_phi=1,spl_order+1
-                do i_data=1,3
-                    splcoe_r(0,:)=spl_data(i_data,1,is_z,is_phi,:,i_z,i_p)
+        do i_phi=1,n_phi
+            do i_r_z=1,spl_order+1
+            do i_r_phi=1,spl_order+1
+                do i_data=1,n_data
+                    splcoe_r(0,:)=spl_data(i_data,1,i_r_z,i_r_phi,:,i_z,i_phi)
                     call spl_reg(spl_order,n_r,h_r,splcoe_r)
                     do k=1,spl_order
-                        spl_data(i_data,k+1,is_z,is_phi,:,i_z,i_p)=splcoe_r(k,:)
+                        spl_data(i_data,k+1,i_r_z,i_r_phi,:,i_z,i_phi)=splcoe_r(k,:)
                     enddo
                 enddo
             enddo
@@ -101,7 +105,151 @@ contains
         enddo
         enddo
 
-    end subroutine spline_transformation
+    end subroutine construct_splines
+
+
+    subroutine eval_splines(spl_data, x, y, dy, d2y)
+        real(8), intent(in) :: spl_data(:,:,:,:,:,:)
+        real(8), intent(in) :: x(3)
+        real(8), intent(inout) :: y, dy(3), d2y(6)
+
+        real(8) :: dr, dz, dphi
+        integer :: nsp1                ! Spline order plus one
+        integer :: i_r, i_z, i_phi, k  ! Counters
+
+        real(8) :: qua,dqua_dr,dqua_dt,dqua_dp
+        real(8) :: d2qua_dr2,d2qua_drdt,d2qua_drdp,d2qua_dt2,d2qua_dtdp,d2qua_dp2
+
+        real(8), dimension(spl_order+1)        :: sp_all,dsp_all_ds,dsp_all_dt
+        real(8), dimension(spl_order+1)        :: d2sp_all_ds2,d2sp_all_dsdt,d2sp_all_dt2
+        real(8), dimension(spl_order+1, spl_order+1) :: stp_all, dstp_all_ds,d2stp_all_ds2
+
+
+        dr=x(1)/h_r
+        i_r=max(0,min(n_r-1,int(dr)))
+        dr=(dr-dble(i_r))*h_r
+        i_r=i_r+1
+
+        dz=x(2)/h_z
+        i_z=max(0,min(n_z-1,int(dz)))
+        dz=(dz-dble(i_z))*h_z
+        i_z=i_z+1
+
+        dphi=modulo(x(3),twopi/dble(nper))/h_phi
+        i_phi=max(0,min(n_phi-1,int(h_phi)))
+        dphi=(h_phi-dble(i_phi))*h_phi
+        i_phi=i_phi+1
+
+        nsp1 = spl_order+1
+
+        !
+        ! Begin interpolation over $x1$
+        !
+        stp_all(1:nsp1,1:nsp1)=spl_data(nsp1,:,:,i_r,i_z,i_phi)
+        dstp_all_ds(1:nsp1,1:nsp1)=stp_all(1:nsp1,1:nsp1)*derf1(nsp1)
+        d2stp_all_ds2(1:nsp1,1:nsp1)=stp_all(1:nsp1,1:nsp1)*derf2(nsp1)
+        !
+        do k=spl_order,3,-1
+            stp_all(1:nsp1,1:nsp1)=spl_data(k,:,:,i_r,i_z,i_phi) &
+                + dr*stp_all(1:nsp1,1:nsp1)
+            dstp_all_ds(1:nsp1,1:nsp1)=spl_data(k,:,:,i_r,i_z,i_phi)*derf1(k) &
+                + dr*dstp_all_ds(1:nsp1,1:nsp1)
+            d2stp_all_ds2(1:nsp1,1:nsp1)=spl_data(k,:,:,i_r,i_z,i_phi)*derf2(k) &
+                + dr*d2stp_all_ds2(1:nsp1,1:nsp1)
+        enddo
+        !
+        stp_all(1:nsp1,1:nsp1)=spl_data(1,:,:,i_r,i_z,i_phi) &
+             + dr*(spl_data(2,:,:,i_r,i_z,i_phi)+dr*stp_all(1:nsp1,1:nsp1))
+        dstp_all_ds(1:nsp1,1:nsp1)=spl_data(2,:,:,i_r,i_z,i_phi) &
+            + dr*dstp_all_ds(1:nsp1,1:nsp1)
+        !
+        ! End interpolation over $x1$
+        !-------------------------------
+        ! Begin interpolation over $x2$
+        !
+        sp_all(1:nsp1)=stp_all(nsp1,1:nsp1)
+        dsp_all_ds(1:nsp1)=dstp_all_ds(nsp1,1:nsp1)
+        d2sp_all_ds2(1:nsp1)=d2stp_all_ds2(nsp1,1:nsp1)
+        dsp_all_dt(1:nsp1)=sp_all(1:nsp1)*derf1(nsp1)
+        d2sp_all_dsdt(1:nsp1)=dsp_all_ds(1:nsp1)*derf1(nsp1)
+        d2sp_all_dt2(1:nsp1)=sp_all(1:nsp1)*derf2(nsp1)
+
+        do k=spl_order,3,-1
+            sp_all(1:nsp1)=stp_all(k,1:nsp1)+dz*sp_all(1:nsp1)
+            dsp_all_ds(1:nsp1)=dstp_all_ds(k,1:nsp1)+dz*dsp_all_ds(1:nsp1)
+            d2sp_all_ds2(1:nsp1)=d2stp_all_ds2(k,1:nsp1)+dz*d2sp_all_ds2(1:nsp1)
+            dsp_all_dt(1:nsp1)=stp_all(k,1:nsp1)*derf1(k)+dz*dsp_all_dt(1:nsp1)
+            d2sp_all_dsdt(1:nsp1)=dstp_all_ds(k,1:nsp1)*derf1(k) &
+                + dz*d2sp_all_dsdt(1:nsp1)
+            d2sp_all_dt2(1:nsp1)=stp_all(k,1:nsp1)*derf2(k)+dz*d2sp_all_dt2(1:nsp1)
+        enddo
+
+        sp_all(1:nsp1)=stp_all(1,1:nsp1) + dz*(stp_all(2,1:nsp1)+dz*sp_all(1:nsp1))
+        dsp_all_ds(1:nsp1)=dstp_all_ds(1,1:nsp1) &
+                            + dz*(dstp_all_ds(2,1:nsp1)+dz*dsp_all_ds(1:nsp1))
+        d2sp_all_ds2(1:nsp1)=d2stp_all_ds2(1,1:nsp1) &
+                            + dz*(d2stp_all_ds2(2,1:nsp1)+dz*d2sp_all_ds2(1:nsp1))
+        dsp_all_dt(1:nsp1)=stp_all(2,1:nsp1)+dz*dsp_all_dt(1:nsp1)
+        d2sp_all_dsdt(1:nsp1)=dstp_all_ds(2,1:nsp1)+dz*d2sp_all_dsdt(1:nsp1)
+        !
+        ! End interpolation over $x2$
+        !--------------------------------
+        ! Begin interpolation over $x3$
+        !
+        qua=sp_all(nsp1)
+        dqua_dr=dsp_all_ds(nsp1)
+        dqua_dt=dsp_all_dt(nsp1)
+        dqua_dp=qua*derf1(nsp1)
+
+        d2qua_dr2=d2sp_all_ds2(nsp1)
+        d2qua_drdt=d2sp_all_dsdt(nsp1)
+        d2qua_drdp=dqua_dr*derf1(nsp1)
+        d2qua_dt2=d2sp_all_dt2(nsp1)
+        d2qua_dtdp=dqua_dt*derf1(nsp1)
+        d2qua_dp2=qua*derf2(nsp1)
+
+        do k=spl_order,3,-1
+            qua=sp_all(k)+dphi*qua
+            dqua_dr=dsp_all_ds(k)+dphi*dqua_dr
+            dqua_dt=dsp_all_dt(k)+dphi*dqua_dt
+            dqua_dp=sp_all(k)*derf1(k)+dphi*dqua_dp
+
+            d2qua_dr2=d2sp_all_ds2(k)+dphi*d2qua_dr2
+            d2qua_drdt=d2sp_all_dsdt(k)+dphi*d2qua_drdt
+            d2qua_drdp=dsp_all_ds(k)*derf1(k)+dphi*d2qua_drdp
+            d2qua_dt2=d2sp_all_dt2(k)+dphi*d2qua_dt2
+            d2qua_dtdp=dsp_all_dt(k)*derf1(k)+dphi*d2qua_dtdp
+            d2qua_dp2=sp_all(k)*derf2(k)+dphi*d2qua_dp2
+        enddo
+
+        qua=sp_all(1)+dphi*(sp_all(2)+dphi*qua)
+        dqua_dr=dsp_all_ds(1)+dphi*(dsp_all_ds(2)+dphi*dqua_dr)
+        dqua_dt=dsp_all_dt(1)+dphi*(dsp_all_dt(2)+dphi*dqua_dt)
+
+        d2qua_dr2=d2sp_all_ds2(1)+dphi*(d2sp_all_ds2(2)+dphi*d2qua_dr2)
+        d2qua_drdt=d2sp_all_dsdt(1)+dphi*(d2sp_all_dsdt(2)+dphi*d2qua_drdt)
+        d2qua_dt2=d2sp_all_dt2(1)+dphi*(d2sp_all_dt2(2)+dphi*d2qua_dt2)
+
+        dqua_dp=sp_all(2)+dphi*dqua_dp
+        d2qua_drdp=dsp_all_ds(2)+dphi*d2qua_drdp
+        d2qua_dtdp=dsp_all_dt(2)+dphi*d2qua_dtdp
+        !
+        ! End interpolation over $x3$
+        !
+
+        y=qua
+
+        dy(1)=dqua_dr
+        dy(2)=dqua_dt
+        dy(3)=dqua_dp
+
+        d2y(1)=d2qua_dr2
+        d2y(2)=d2qua_drdt
+        d2y(3)=d2qua_drdp
+        d2y(4)=d2qua_dt2
+        d2y(5)=d2qua_dtdp
+        d2y(6)=d2qua_dp2
+    end subroutine eval_splines
 
 
     subroutine get_transformation(delta_phi, chi_gauge)
@@ -113,26 +261,26 @@ contains
 
         real(8), allocatable :: y(:), dy(:)
         real(8) :: r1, r2
-        integer :: i_r, i_p, i_z, i_ctr
+        integer :: i_r, i_z, i_phi, i_ctr
 
         i_ctr=0
 
-        !$omp parallel private(y, dy, i_r, i_p, i_z, r1, r2)
+        !$omp parallel private(y, dy, i_r, i_z, i_phi, r1, r2)
         !$omp critical
             allocate(y(ndim),dy(ndim))
         !$omp end critical
         !$omp do
-        do i_z=1,n_z
+        do i_phi=1,n_phi
             !$omp critical
             i_ctr = i_ctr + 1
-            print *,'integrate ODE: ',i_ctr,' of ',n_z
+            print *,'integrate ODE: ',i_ctr,' of ',n_phi
             !$omp end critical
-            z_c = zmin + h_z*dble(i_z-1)
-            do i_p=1,n_phi
-                phi_c = h_phi*dble(i_p-1)
+            phi_c = h_phi*dble(i_phi-1)
+            do i_z=1,n_z
+                z_c = zmin + h_z*dble(i_z-1)
 
-                delta_phi(1, i_p, i_z) = 0.d0
-                chi_gauge(1, i_p, i_z) = 0.d0
+                delta_phi(1, i_z, i_phi) = 0.d0
+                chi_gauge(1, i_z, i_phi) = 0.d0
 
                 y = 0.d0
 
@@ -142,8 +290,8 @@ contains
 
                     call odeint_allroutines(y, ndim, r1, r2, relerr, rh_ran)
 
-                    delta_phi(i_r, i_p, i_z) = y(1)
-                    chi_gauge(i_r, i_p, i_z) = y(2)
+                    delta_phi(i_r, i_z, i_phi) = y(1)
+                    chi_gauge(i_r, i_z, i_phi) = y(2)
                 enddo
             enddo
         enddo
@@ -166,7 +314,7 @@ contains
             Ar, Ap, Az)
 
         dy(1) = -Br/Bp         ! Must still be divided by r_c for covariant Bp
-        dy(2) = Ar + Ap*dy(1)  ! Here it is reused so that r_c cancels out
+        dy(2) = Ar + Ap*dy(1)  ! Here it i_r reused so that r_c cancels out
         dy(1) = dy(1) / r_c    ! Finally we divide by r_c
 
     end subroutine rh_ran
--- main.f90
+++ main.f90
@@ -1,35 +1,64 @@
 program main
-    use canonical, only : init, get_transformation, spline_transformation, &
-                          spl_order
+    use canonical, only : init, get_transformation, construct_splines, spl_order
 
     implicit none
 
-    integer, parameter :: n_r=50, n_phi=64, n_z=75
+    integer, parameter :: n_r=50, n_z=75, n_phi=64
     real(8), parameter :: rmin = 75.d0, rmax = 264.42281879194627d0, &
                           zmin = -150.d0, zmax = 147.38193979933115d0
 
-    real(8), dimension(:,:,:), allocatable :: delta_phi, chi_gauge
-    real(8), dimension(:,:,:,:,:,:,:), allocatable :: spl_data
+    real(8), dimension(:,:,:), allocatable :: lam_phi, chi_gauge
+    real(8), dimension(:,:,:,:,:,:,:), allocatable :: spl_lam_chi
+    real(8), dimension(:,:,:,:,:,:,:), allocatable :: spl_magfie  ! A2, A3, h2, h3, Bmod
     integer :: outfile_unit
 
-    allocate(delta_phi(n_r, n_phi, n_z), chi_gauge(n_r, n_phi, n_z))
-    allocate(spl_data(2,spl_order+1,spl_order+1,spl_order+1,n_r,n_z,n_phi))
+    allocate(lam_phi(n_r, n_z, n_phi), chi_gauge(n_r, n_z, n_phi))
+    allocate(spl_lam_chi(2,spl_order+1,spl_order+1,spl_order+1,n_r,n_z,n_phi))
+    allocate(spl_magfie(5,spl_order+1,spl_order+1,spl_order+1,n_r,n_z,n_phi))
 
-    call init(n_r, n_phi, n_z)
-    call get_transformation(delta_phi, chi_gauge)
+    call init(n_r, n_z, n_phi)
+    call get_transformation(lam_phi, chi_gauge)
 
-    open(newunit=outfile_unit, file="delta_phi.out")
-        write(outfile_unit, *) delta_phi
+    open(newunit=outfile_unit, file="lam_phi.out")
+        write(outfile_unit, *) lam_phi
     close(outfile_unit)
 
     open(newunit=outfile_unit, file="chi_gauge.out")
         write(outfile_unit, *) chi_gauge
     close(outfile_unit)
 
-    call spline_transformation(spl_data)
+    spl_lam_chi(1,1,1,1,:,:,:) = lam_phi
+    spl_lam_chi(2,1,1,1,:,:,:) = chi_gauge
+    ! deallocate(lam_phi, chi_gauge)
 
-    deallocate(delta_phi, chi_gauge)
-    deallocate(spl_data)
+    call construct_splines(spl_lam_chi)
+
+    call test_splines
+
+    ! TODO: Compute A, h, Bmod from splines on grid in canonical coordinates
+    ! TODO: Spline A, h, Bmod on grid in canonical coordinates
+
+    deallocate(spl_lam_chi, spl_magfie)
+
+contains
+
+    subroutine test_splines
+        use canonical, only : eval_splines
+
+        real(8) :: x(3)
+        real(8) :: y, dy(3), d2y(6)
+
+        x(1) = rmin
+        x(2) = zmin
+        x(3) = 0.d0
+
+        call eval_splines(spl_lam_chi(1,:,:,:,:,:,:), x, y, dy, d2y)
+
+        print *, "y = ", y, lam_phi(1,1,1)
 
 
+        call eval_splines(spl_lam_chi(2,:,:,:,:,:,:), x, y, dy, d2y)
+
+        print *, "y = ", y, chi_gauge(1,1,1)
+    end subroutine test_splines
 end program main
