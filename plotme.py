5172ae65e053d70d2f8fa24cdddb936c7ff41606
Christopher Albert <albert@alumni.tugraz.at>
2024-01-16 01:57:20 +0100

    Correct plots

--- plotme.py
+++ plotme.py
@@ -2,26 +2,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-shape = (75, 64, 50)
+shape = (64, 75, 50)
 
 # %% Plot delta_phi
 
-data = np.loadtxt("delta_phi.out").reshape(shape)
+data = np.loadtxt("lam_phi.out").reshape(shape)
 
 # Plot 2D slice as contour
 plt.figure()
 plt.contour(data[25, :, :])
 plt.colorbar()
-plt.title("delta_phi")
+plt.title("lam_phi")
 
 plt.figure()
 plt.contour(data[:, :, 25])
 plt.colorbar()
-plt.title("delta_phi")
+plt.title("lam_phi")
 
 plt.figure()
 plt.plot(data[25, :, 25], 'o-')
-plt.title("delta_phi")
+plt.title("lam_phi")
 
 # %% Plot gauge function chi
 
