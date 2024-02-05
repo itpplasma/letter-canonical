f807e74dd02cf45c7a4b9f7e284f38497e8ca703
Christopher Albert <albert@alumni.tugraz.at>
2024-02-05 11:37:06 +0000

    Fixed splines

--- main.f90
+++ main.f90
@@ -1,5 +1,5 @@
 program main
-    use interpolate, only : SplineData3D, construct_splines_3d, evaluate_splines_3d
+    use interpolate, only : SplineData3D, construct_splines_3d, evaluate_splines_3d, destroy_splines_3d, disp
     use canonical, only : init_canonical, get_transformation, construct_splines, spl_order, twopi
 
     implicit none
@@ -9,32 +9,14 @@ program main
     real(8), parameter :: rmin = 75.d0, rmax = 264.42281879194627d0, &
                           zmin = -150.d0, zmax = 147.38193979933115d0
 
-    real(8), dimension(:,:,:), allocatable :: lam_phi, chi_gauge, test_function
-    real(8), dimension(:,:,:,:,:,:,:), allocatable :: spl_lam_chi
-    real(8), dimension(:,:,:,:,:,:,:), allocatable :: spl_magfie  ! A2, A3, h2, h3, Bmod
+    real(8), dimension(:,:,:), allocatable :: lam_phi, chi_gauge
 
-    real(8), dimension(:), allocatable :: r, z, phi
+    integer :: outfile_unit
 
-    integer :: kr, kz, kphi, outfile_unit
+    type(SplineData3D) :: spl_lam, spl_chi
+    ! type(SplineData3D) :: spl_A2, spl_A3, spl_h2, spl_h3, spl_Bmod
 
-    type(SplineData3D) :: spl
-
-    allocate(lam_phi(n_r, n_z, n_phi), chi_gauge(n_r, n_z, n_phi), test_function(n_r, n_z, n_phi))
-    allocate(spl_lam_chi(3,spl_order+1,spl_order+1,spl_order+1,n_r,n_z,n_phi))
-    allocate(spl_magfie(5,spl_order+1,spl_order+1,spl_order+1,n_r,n_z,n_phi))
-    allocate(r(n_r), z(n_z), phi(n_phi))
-
-    do kr = 1, n_r
-        r(kr) = rmin + (rmax - rmin) * (kr - 1) / (n_r - 1)
-    end do
-
-    do kz = 1, n_z
-        z(kz) = zmin + (zmax - zmin) * (kz - 1) / (n_z - 1)
-    end do
-
-    do kphi = 1, n_phi
-        phi(kphi) = twopi * (kphi - 1) / n_phi
-    end do
+    allocate(lam_phi(n_r, n_z, n_phi), chi_gauge(n_r, n_z, n_phi))
 
     call init_canonical(n_r, n_z, n_phi)
     call get_transformation(lam_phi, chi_gauge)
@@ -47,127 +29,59 @@ program main
         write(outfile_unit, *) chi_gauge
     close(outfile_unit)
 
-    do kr = 1, n_r
-        do kz = 1, n_z
-            do kphi = 1, n_phi
-                test_function(kr, kz, kphi) = r(kr)*z(kz)*cos(phi(kphi))
-            end do
-        end do
-    end do
+    call construct_splines_3d( &
+        [rmin, zmin, 0.d0], [rmax, zmax, twopi], lam_phi, &
+        [3, 3, 3], [.False., .False., .True.], spl_lam)
+    deallocate(lam_phi)
 
-    print *, "test_function(1,1,1) = ", test_function(1,1,1)
-
-    open(newunit=outfile_unit, file="test_function.out")
-        write(outfile_unit, *) test_function
-    close(outfile_unit)
-
-    spl_lam_chi(1,1,1,1,:,:,:) = lam_phi
-    spl_lam_chi(2,1,1,1,:,:,:) = chi_gauge
-    spl_lam_chi(3,1,1,1,:,:,:) = test_function
-
-    ! deallocate(lam_phi, chi_gauge)
-
-    call construct_splines(spl_lam_chi)
-
+    call construct_splines_3d( &
+        [rmin, zmin, 0.d0], [rmax, zmax, twopi], chi_gauge, &
+        [3, 3, 3], [.False., .False., .True.], spl_chi)
+    deallocate(chi_gauge)
 
     call test_splines
-    call test_spline_derivatives
+
 
     ! TODO: Compute A, h, Bmod from splines on grid in canonical coordinates
     ! TODO: Spline A, h, Bmod on grid in canonical coordinates
 
-    deallocate(spl_lam_chi, spl_magfie)
-
 contains
 
     subroutine test_splines
-        use canonical, only : eval_splines_der2
 
-        real(8) :: x(3)
-        real(8) :: y, dy(3), d2y(6)
+        integer, parameter :: n_r_test=49, n_z_test=74, n_phi_test=63
 
-        integer :: kr, kz, kphi
+        real(8), dimension(:,:,:), allocatable :: lam_test, chi_test
+        real(8), dimension(3) :: x
 
-        kr=10
-        kz=20
-        kphi=30
+        integer :: i_r, i_z, i_phi
 
-        x(1) = r(kr)
-        x(2) = z(kz)
-        x(3) = phi(kphi)
+        allocate(lam_test(n_r_test, n_z_test, n_phi_test))
+        allocate(chi_test(n_r_test, n_z_test, n_phi_test))
 
-        call eval_splines_der2(spl_lam_chi(1,:,:,:,:,:,:), x, y, dy, d2y)
-
-        print *, "lam_phi = ", lam_phi(kr, kz, kphi), y
-
-
-        call eval_splines_der2(spl_lam_chi(2,:,:,:,:,:,:), x, y, dy, d2y)
-
-        print *, "chi_gauge = ", chi_gauge(kr, kz, kphi), y
-
-        call eval_splines_der2(spl_lam_chi(3,:,:,:,:,:,:), x, y, dy, d2y)
-
-        print *, "test = ", r(kr)*z(kz)*cos(phi(kphi)), y
-
-        call construct_splines_3d([rmin, zmin, 0.0d0], [rmax, zmax, twopi], &
-        test_function, order=[3, 3, 3], periodic=[.False., .False., .True.], spl=spl)
-
-        call evaluate_splines_3d(x, spl, y)
-
-        print *, "test libneo = ", y
-
-        do kphi = 1, n_phi
-            x(3) = phi(kphi)
-            do kz = 1, n_z
-                x(2) = z(kz)
-                do kr = 1, n_r
-                    x(1) = r(kr)
-                    call eval_splines_der2(spl_lam_chi(1,:,:,:,:,:,:), x, y, dy, d2y)
-                    lam_phi(kr, kz, kphi) = y
-                    call eval_splines_der2(spl_lam_chi(2,:,:,:,:,:,:), x, y, dy, d2y)
-                    chi_gauge(kr, kz, kphi) = y
-                    call eval_splines_der2(spl_lam_chi(3,:,:,:,:,:,:), x, y, dy, d2y)
-                    test_function(kr, kz, kphi) = y
+        do i_phi = 1, n_phi_test
+            do i_z = 1, n_z_test
+                do i_r = 1, n_r_test
+                    x = [rmin + (rmax - rmin) * (i_r - 1) / (n_r_test - 1), &
+                         zmin + (zmax - zmin) * (i_z - 1) / (n_z_test - 1), &
+                         twopi * (i_phi - 1) / (n_phi_test - 1)]
+                    call evaluate_splines_3d( &
+                        x, spl_lam, lam_test(i_r, i_z, i_phi))
+                    call evaluate_splines_3d( &
+                        x, spl_chi, chi_test(i_r, i_z, i_phi))
                 end do
             end do
         end do
 
-        open(newunit=outfile_unit, file="lam_phi_splined.out")
-            write(outfile_unit, *) lam_phi
+        open(newunit=outfile_unit, file="lam_spl.out")
+            write(outfile_unit, *) lam_test
         close(outfile_unit)
 
-        open(newunit=outfile_unit, file="chi_gauge_splined.out")
-            write(outfile_unit, *) chi_gauge
+        open(newunit=outfile_unit, file="chi_spl.out")
+            write(outfile_unit, *) chi_test
         close(outfile_unit)
 
-        open(newunit=outfile_unit, file="test_function_splined.out")
-            write(outfile_unit, *) test_function
-        close(outfile_unit)
+        deallocate(lam_test, chi_test)
     end subroutine test_splines
 
-    subroutine test_spline_derivatives
-        use canonical, only : eval_splines_der2
-
-        real(8) :: x(3)
-        real(8) :: y, dy(3), d2y(6)
-
-        integer :: kr, kz, kphi
-
-        kr=23
-        kz=32
-        kphi=25
-
-        x(1) = r(kr)
-        x(2) = z(kz)
-        x(3) = phi(kphi)
-
-        call eval_splines_der2(spl_lam_chi(3,:,:,:,:,:,:), x, y, dy, d2y)
-
-        print *, "d/dr test   = ", z(kz)*cos(phi(kphi)), dy(1)
-        print *, "d/dz test   = ", r(kr)*cos(phi(kphi)), dy(2)
-        print *, "d/dphi test = ", -r(kr)*z(kz)*sin(phi(kphi)), dy(3)
-
-        print *, "d2/dr2 test   = ", 0.0, d2y(1)
-
-    end subroutine test_spline_derivatives
 end program main
--- plot_transformation.py
+++ plot_transformation.py
@@ -2,42 +2,40 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from scipy.interpolate import RegularGridInterpolator
+
 shape = (64, 75, 50)
+shape_spl = (63, 74, 49)
 
 def doplot(data, title):
     # Plot 2D slice as contour
     plt.figure()
-    plt.contour(data[:, 25, :])
+    plt.contour(data[:, 1, :])
     plt.colorbar()
     plt.title(title)
 
     plt.figure()
-    plt.contour(data[:, :, 25])
+    plt.contour(data[:, :, 1])
     plt.colorbar()
     plt.title(title)
 
     plt.figure()
-    plt.plot(data[:, 25, 25], 'o-')
+    plt.plot(data[:, 1, 1], 'o-')
+    plt.plot(data[:, 2, 2], 'o-')
     plt.title(title)
 
 
 data = np.loadtxt("lam_phi.out").reshape(shape)
 doplot(data, "lam_phi")
 
-data = np.loadtxt("lam_phi_splined.out").reshape(shape)
+data = np.loadtxt("lam_spl.out").reshape(shape_spl)
 doplot(data, "lam_phi_splined")
 
 data = np.loadtxt("chi_gauge.out").reshape(shape)
 doplot(data, "chi_gauge")
 
-data = np.loadtxt("chi_gauge_splined.out").reshape(shape)
+data = np.loadtxt("chi_spl.out").reshape(shape_spl)
 doplot(data, "chi_gauge_splined")
-
-data = np.loadtxt("test_function.out").reshape(shape)
-doplot(data, "test_function")
-
-data = np.loadtxt("test_function_splined.out").reshape(shape)
-doplot(data, "test_function_splined")
 
 
 # %%
