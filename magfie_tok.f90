7914a3a4582f10b949759c02c5621335850e551a
Christopher Albert <albert@alumni.tugraz.at>
2024-03-23 03:44:19 +0000

    Indent

--- magfie_tok.f90
+++ magfie_tok.f90
@@ -113,90 +113,90 @@ module magfie_tok
     subroutine my_field_divfree(r,phi,z,Br,Bp,Bz,dBrdR,dBrdp,dBrdZ    &
                             ,dBpdR,dBpdp,dBpdZ,dBzdR,dBzdp,dBzdZ,Ar,Ap,Az)
 
-    use bdivfree_mod, only : nr,nz,ntor,icp,ipoint,hr,hz,pfac,&
-        & rpoi,zpoi,apav
-    use libneo_kinds, only : complex_kind, real_kind
+        use bdivfree_mod, only : nr,nz,ntor,icp,ipoint,hr,hz,pfac,&
+            & rpoi,zpoi,apav
+        use libneo_kinds, only : complex_kind, real_kind
 
-    implicit none
+        implicit none
 
-    double precision, intent(in) :: r, phi, z
-    double precision, intent(inout) :: Br, Bp, Bz, dBrdR, dBrdp, dBrdZ,   &
-                                     dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ,   &
-                                     Ar, Ap, Az
+        double precision, intent(in) :: r, phi, z
+        double precision, intent(inout) :: Br, Bp, Bz, dBrdR, dBrdp, dBrdZ,   &
+                                        dBpdR, dBpdp, dBpdZ, dBzdR, dBzdp, dBzdZ,   &
+                                        Ar, Ap, Az
 
-    integer :: n,ierr
-    real(kind=real_kind) :: f,fr,fz,frr,frz,fzz
-    real(kind=real_kind) :: delBr,delBz,delBp
-    real(kind=real_kind) :: deldBrdR,deldBrdp,deldBrdZ
-    real(kind=real_kind) :: deldBpdR,deldBpdp,deldBpdZ
-    real(kind=real_kind) :: deldBzdR,deldBzdp,deldBzdZ
-    complex(kind=complex_kind) :: expon,anr,anz,anr_r,anr_z,anz_r,anz_z
-    complex(kind=complex_kind) :: anr_rr,anr_rz,anr_zz,anz_rr,anz_rz,anz_zz
-    complex(kind=complex_kind) :: pfac_imaginary
+        integer :: n,ierr
+        real(kind=real_kind) :: f,fr,fz,frr,frz,fzz
+        real(kind=real_kind) :: delBr,delBz,delBp
+        real(kind=real_kind) :: deldBrdR,deldBrdp,deldBrdZ
+        real(kind=real_kind) :: deldBpdR,deldBpdp,deldBpdZ
+        real(kind=real_kind) :: deldBzdR,deldBzdp,deldBzdZ
+        complex(kind=complex_kind) :: expon,anr,anz,anr_r,anr_z,anz_r,anz_z
+        complex(kind=complex_kind) :: anr_rr,anr_rz,anr_zz,anz_rr,anz_rz,anz_zz
+        complex(kind=complex_kind) :: pfac_imaginary
 
 
-    Bp = 0.0d0
-    ! TODO: Solve this with a vector potential being Az = -int Bp dr with gauge Ar=0
-    !
-    ! call spline(nr,nz,rpoi,zpoi,hr,hz,icp,rbpav_coef,ipoint,r,z,         &
-    !             f,fr,fz,frr,frz,fzz,ierr)
-    ! Bp = f/r
-    ! dBpdR = fr/r - Bp/r
-    ! dBpdZ = fz/r
-    ! dBpdp = 0.d0
+        Bp = 0.0d0
+        ! TODO: Solve this with a vector potential being Az = -int Bp dr with gauge Ar=0
+        !
+        ! call spline(nr,nz,rpoi,zpoi,hr,hz,icp,rbpav_coef,ipoint,r,z,         &
+        !             f,fr,fz,frr,frz,fzz,ierr)
+        ! Bp = f/r
+        ! dBpdR = fr/r - Bp/r
+        ! dBpdZ = fz/r
+        ! dBpdp = 0.d0
 
-    call spline(nr,nz,rpoi,zpoi,hr,hz,icp,apav,ipoint,r,z,         &
-                f,fr,fz,frr,frz,fzz,ierr)
+        call spline(nr,nz,rpoi,zpoi,hr,hz,icp,apav,ipoint,r,z,         &
+                    f,fr,fz,frr,frz,fzz,ierr)
 
-    Br=-fz/r
-    Bz=fr/r
-    dBrdR=fz/r**2-frz/r
-    dBrdZ=-fzz/r
-    dBzdR=-fr/r**2+frr/r
-    dBzdZ=frz/r
-    dBrdp=0.d0
-    dBzdp=0.d0
+        Br=-fz/r
+        Bz=fr/r
+        dBrdR=fz/r**2-frz/r
+        dBrdZ=-fzz/r
+        dBzdR=-fr/r**2+frr/r
+        dBzdZ=frz/r
+        dBrdp=0.d0
+        dBzdp=0.d0
 
-    Ar = 0.d0
-    Ap = 0.d0
-    Az = 0.d0
+        Ar = 0.d0
+        Ap = 0.d0
+        Az = 0.d0
 
-    do n=1,ntor
-        call spline_vector_potential_n(n, r, z, anr,anz,anr_r,anr_z,anz_r,anz_z, &
-        anr_rr,anr_rz,anr_zz,anz_rr,anz_rz,anz_zz)
+        do n=1,ntor
+            call spline_vector_potential_n(n, r, z, anr,anz,anr_r,anr_z,anz_r,anz_z, &
+            anr_rr,anr_rz,anr_zz,anz_rr,anz_rz,anz_zz)
 
-        pfac_imaginary = cmplx(0.0, pfac, kind=complex_kind)
+            pfac_imaginary = cmplx(0.0, pfac, kind=complex_kind)
 
-        expon=exp(cmplx(0.d0,n*pfac*phi, kind=complex_kind))
-        delBr=2.d0*real(pfac_imaginary*n*anz*expon/r, kind=real_kind)
-        delBz=-2.d0*real(pfac_imaginary*n*anr*expon/r, kind=real_kind)
-        delBp=2.d0*real((anr_z-anz_r)*expon, kind=real_kind)
-        deldBrdR=-delbr/r+2.d0*real(pfac_imaginary*n*anz_r*expon/r, kind=real_kind)
-        deldBrdZ=2.d0*real(pfac_imaginary*n*anz_z*expon/r, kind=real_kind)
-        deldBrdp=-2.d0*real((pfac*n)**2*anz*expon/r, kind=real_kind)
-        deldBzdR=-delbz/r-2.d0*real(pfac_imaginary*n*anr_r*expon/r, kind=real_kind)
-        deldBzdZ=-2.d0*real(pfac_imaginary*n*anr_z*expon/r, kind=real_kind)
-        deldBzdp=2.d0*real((pfac*n)**2*anr*expon/r, kind=real_kind)
-        deldBpdR=2.d0*real((anr_rz-anz_rr)*expon, kind=real_kind)
-        deldBpdZ=2.d0*real((anr_zz-anz_rz)*expon, kind=real_kind)
-        deldBpdp=2.d0*real(pfac_imaginary*n*(anr_z-anz_r)*expon, kind=real_kind)
+            expon=exp(cmplx(0.d0,n*pfac*phi, kind=complex_kind))
+            delBr=2.d0*real(pfac_imaginary*n*anz*expon/r, kind=real_kind)
+            delBz=-2.d0*real(pfac_imaginary*n*anr*expon/r, kind=real_kind)
+            delBp=2.d0*real((anr_z-anz_r)*expon, kind=real_kind)
+            deldBrdR=-delbr/r+2.d0*real(pfac_imaginary*n*anz_r*expon/r, kind=real_kind)
+            deldBrdZ=2.d0*real(pfac_imaginary*n*anz_z*expon/r, kind=real_kind)
+            deldBrdp=-2.d0*real((pfac*n)**2*anz*expon/r, kind=real_kind)
+            deldBzdR=-delbz/r-2.d0*real(pfac_imaginary*n*anr_r*expon/r, kind=real_kind)
+            deldBzdZ=-2.d0*real(pfac_imaginary*n*anr_z*expon/r, kind=real_kind)
+            deldBzdp=2.d0*real((pfac*n)**2*anr*expon/r, kind=real_kind)
+            deldBpdR=2.d0*real((anr_rz-anz_rr)*expon, kind=real_kind)
+            deldBpdZ=2.d0*real((anr_zz-anz_rz)*expon, kind=real_kind)
+            deldBpdp=2.d0*real(pfac_imaginary*n*(anr_z-anz_r)*expon, kind=real_kind)
 
-        br=br+delbr
-        bz=bz+delbz
-        bp=bp+delbp
-        dBrdR=dBrdR+deldBrdR
-        dBrdZ=dBrdZ+deldBrdZ
-        dBrdp=dBrdp+deldBrdp
-        dBzdR=dBzdR+deldBzdR
-        dBzdZ=dBzdZ+deldBzdZ
-        dBzdp=dBzdp+deldBzdp
-        dBpdR=dBpdR+deldBpdR
-        dBpdZ=dBpdZ+deldBpdZ
-        dBpdp=dBpdp+deldBpdp
+            br=br+delbr
+            bz=bz+delbz
+            bp=bp+delbp
+            dBrdR=dBrdR+deldBrdR
+            dBrdZ=dBrdZ+deldBrdZ
+            dBrdp=dBrdp+deldBrdp
+            dBzdR=dBzdR+deldBzdR
+            dBzdZ=dBzdZ+deldBzdZ
+            dBzdp=dBzdp+deldBzdp
+            dBpdR=dBpdR+deldBpdR
+            dBpdZ=dBpdZ+deldBpdZ
+            dBpdp=dBpdp+deldBpdp
 
-        Ar = Ar + 2.d0*real(anr*expon, kind=real_kind)
-        Az = Az + 2.d0*real(anz*expon, kind=real_kind)
-    end do
+            Ar = Ar + 2.d0*real(anr*expon, kind=real_kind)
+            Az = Az + 2.d0*real(anz*expon, kind=real_kind)
+        end do
 
     end subroutine my_field_divfree
 
