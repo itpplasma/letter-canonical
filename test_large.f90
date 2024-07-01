8fbeb6a31398ab054cbd0d684d0b5ff48d4d6076
Christopher Albert <albert@tugraz.at>
2024-07-01 23:57:09 +0200

    Fix test

--- test_large.f90
+++ test_large.f90
@@ -3,7 +3,7 @@ program test_large
 
     implicit none
 
-    integer, parameter :: n_r=100, n_z=75, n_phi=64
+    integer, parameter :: n_r=100, n_phi=64, n_z=75
 
     call setup
     call test_physical_field
@@ -21,8 +21,8 @@ contains
         rmax = 264.42281879194627d0
         zmin = -150.d0
         zmax = 147.38193979933115d0
-        call init_canonical(n_r, n_z, n_phi, [rmin, zmin, 0.0d0], &
-            [rmax, zmax, twopi], TestFieldType())
+        call init_canonical(n_r, n_phi, n_z, [rmin, 0.0d0, zmin], &
+            [rmax, twopi, zmax], TestFieldType())
         call print_ok
     end subroutine setup
 
@@ -43,32 +43,27 @@ contains
         real(8) :: BR_expected, BZ_expected, Bphi_expected, B_pitch_expected(2)
         real(8) :: x(3), gii(3), sqrtg
 
-        allocate(xcyl(3,n_r,n_z,n_phi))
-        allocate(B(3,n_r,n_z,n_phi), Acyl(3,n_r,n_z,n_phi))
+        allocate(xcyl(3, n_r, n_phi, n_z))
+        allocate(B(3, n_r, n_phi, n_z), Acyl(3, n_r, n_phi, n_z))
 
         call print_test("test_physical_field")
 
         call generate_regular_grid(xcyl)
         call get_physical_field(xcyl, B, Acyl)
 
-        call construct_splines_3d(x_min, x_max, &
-            B(1,:,:,:), order, periodic, spl_BR)
-        call construct_splines_3d(x_min, x_max, &
-            B(2,:,:,:), order, periodic, spl_BZ)
-        call construct_splines_3d(x_min, x_max, &
-            B(3,:,:,:), order, periodic, spl_Bphi)
+        call construct_splines_3d(x_min, x_max, B(1,:,:,:), order, periodic, spl_BR)
+        call construct_splines_3d(x_min, x_max, B(2,:,:,:), order, periodic, spl_Bphi)
+        call construct_splines_3d(x_min, x_max, B(3,:,:,:), order, periodic, spl_BZ)
         deallocate(B)
 
-        call construct_splines_3d(x_min, x_max, &
-            Acyl(1,:,:,:), order, periodic, spl_AR)
-        call construct_splines_3d(x_min, x_max, &
-            Acyl(2,:,:,:), order, periodic, spl_AZ)
-        call construct_splines_3d(x_min, x_max, &
-            Acyl(3,:,:,:)*xcyl(1,:,:,:), order, periodic, spl_Aphicov)
+        call construct_splines_3d(x_min, x_max, Acyl(1,:,:,:), order, periodic, spl_AR)
+        call construct_splines_3d(x_min, x_max, Acyl(2,:,:,:)*xcyl(1,:,:,:), order, &
+            periodic, spl_Aphicov)
+        call construct_splines_3d(x_min, x_max, Acyl(3,:,:,:), order, periodic, spl_AZ)
         deallocate(Acyl)
 
         x = 0.5d0*(x_min + x_max)
-        x(3) = 0.2d0
+        x(2) = 0.2d0
         call evaluate_splines_3d(spl_BR, x, BR)
         call evaluate_splines_3d(spl_BZ, x, BZ)
         call evaluate_splines_3d(spl_Bphi, x, Bphi)
@@ -76,12 +71,12 @@ contains
         call evaluate_splines_3d_der2(spl_AZ, x, AZ, dAZ, dummy)
         call evaluate_splines_3d_der2(spl_Aphicov, x, Aphicov, dAphicov, dummy)
 
-        gii = [1.d0, 1.d0, x(1)**2]
+        gii = [1.d0, x(1)**2, 1.d0]
         sqrtg = sqrt(gii(1)*gii(2)*gii(3))
 
-        BR_expected = (dAphicov(2) - dAZ(3))/sqrtg*sqrt(gii(1))
-        BZ_expected = (dAR(3) - dAphicov(1))/sqrtg*sqrt(gii(2))
-        Bphi_expected = (dAZ(1) - dAR(2))/sqrtg*sqrt(gii(3))
+        BR_expected = (dAZ(2) - dAphicov(3))/sqrtg*sqrt(gii(1))
+        Bphi_expected = (dAR(2) - dAZ(1))/sqrtg*sqrt(gii(2))
+        BZ_expected = (dAphicov(1) - dAR(3))/sqrtg*sqrt(gii(3))
 
         B_pitch_expected(1) = BR_expected/Bphi_expected
         B_pitch_expected(2) = BZ_expected/Bphi_expected
