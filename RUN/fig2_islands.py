06c2c1178827b02bade85f386ad1553225864e53
Christopher Albert <albert@tugraz.at>
2024-07-27 22:02:14 +0200

    Plotting Fig. 2 islands with Python

--- RUN/fig2_islands.py
+++ RUN/fig2_islands.py
@@ -3,24 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def load_banana(prefix):
-    data = np.loadtxt(prefix + '.out')
+    data = np.loadtxt(prefix + '_phi0minus.out')
     tau = data[:,0]
     z = data[:,1:]
     return tau, z
 
-tau_euler, z_euler = load_banana('fig1_expl_impl_euler')
-tau_rk45_cyl, z_rk45_cyl = load_banana('fig1_rk45_cyl')
-tau_rk45_can, z_rk45_can = load_banana('fig1_rk45_can')
+tau_euler, z_euler = load_banana('fig2_expl_impl_euler')
+tau_rk45_cyl, z_rk45_cyl = load_banana('fig2_rk45_cyl')
+tau_rk45_can, z_rk45_can = load_banana('fig2_rk45_can')
 
 plt.figure(figsize=(3.5,5))
-plt.plot(z_rk45_cyl[:,0], z_rk45_cyl[:,2], '-', color='darkgray')
-plt.plot(z_rk45_can[:,0], z_rk45_can[:,2], '-', color='lightgray')
+plt.plot(z_rk45_cyl[:,0], z_rk45_cyl[:,2], ',', color='darkgray')
+plt.plot(z_rk45_can[:,0], z_rk45_can[:,2], ',', color='lightgray')
 plt.plot(z_euler[:,0], z_euler[:,2], ',', color='black')
 plt.xlabel(r'$R$ / cm')
 plt.ylabel(r'$Z$ / cm')
 
-plt.xticks([100, 150, 200, 250])
-plt.xlim([100, 250])
-plt.ylim([-132, 100])
+plt.xlim([125, 210])
+plt.ylim([-55, 65])
 
 # %%
