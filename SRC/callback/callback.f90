c2536678171c6793a00214fdf05bc38687ee452c
Christopher Albert <albert@alumni.tugraz.at>
2024-08-08 11:47:49 +0200

    Sergei update fig

--- SRC/callback/callback.f90
+++ SRC/callback/callback.f90
@@ -1,6 +1,18 @@
 module callback
-    use, intrinsic :: iso_fortran_env, only: dp => real64
-    use callback_base
-    use cut_callback
+    use callback_base, only: callback_t, callback_pointer_t
+    use cut_callback, only: cut_callback_t
     implicit none
+
+    contains
+
+    function create_callback(callback_type) result(new_callback)
+        class(callback_t), allocatable :: new_callback
+        character(*) :: callback_type
+        select case(callback_type)
+        case('cut')
+            new_callback = cut_callback_t()
+        case default
+            error stop 'create_callback: Unknown callback type'
+        end select
+    end function create_callback
 end module callback
